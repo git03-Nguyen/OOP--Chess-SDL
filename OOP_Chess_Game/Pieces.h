778fa57c036bbcabb46800dd67f1f8b1317cc697
Thai Chi Vy <96096966+gaizzzzz@users.noreply.github.com>
2023-05-12 20:17:04 +0700

    Update Pieces.h
    Add method getTexture()

--- OOP_Chess_Game/Pieces.h
+++ OOP_Chess_Game/Pieces.h
@@ -76,6 +76,7 @@ public:
 	bool getDead() const;
 	bool getChosen() const;
 	PieceType getType() const;
+	Texture* getTexture();
 
 	virtual Piece* move(const Coordinate& c) = 0;
 	virtual std::vector<Coordinate> getPossibleMoves() const = 0;
