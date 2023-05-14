4dad9c89912e281a0fb31d630a4ee1e5680cec69
TreeDude03 <94376370+TreeDude03@users.noreply.github.com>
2023-05-15 00:52:16 +0700

    Update GUI.cpp

--- OOP_Chess_Game/GUI.cpp
+++ OOP_Chess_Game/GUI.cpp
@@ -1,27 +1,16 @@
 #include "GUI.h"
 #include <iostream>
 
-//void SDL_Delay(Uint32 ms);
-//
+
 GUI::GUI() {
 	this->active = true;
+	this->background = Image({ 0,0,1000,600 }, "..\\Assets\\chess\\back.jpg");
 }
-
 GUI::~GUI() {
 }
 
-void GUI::renderBackground(SDL_Renderer* renderer) {
-	SDL_Surface* image = IMG_Load(this->pathImageBackground.c_str());
-	if (image) {
-		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
-		SDL_FreeSurface(image);
-		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-		SDL_RenderPresent(renderer);
-	}
-	else std::cout << "can't load";
-}
-void GUI::clear(SDL_Renderer* renderer) {
-	SDL_RenderClear(renderer);
+void GUI::renderBackground() {
+	this->background.renderImage();
 }
 bool GUI::isActive() {
 	return this->active;
@@ -35,7 +24,10 @@ void GUI::deactivate() {
 TypeGUI GUI::getTypeGUI() const {
 	return TypeGUI();
 }
-//
+void GUI::clear() { //clear 
+	SDL_RenderClear(Window::renderer);
+}
+// 
 //TypeGUI MenuGUI::getTypeGUI() const {
 //	return TypeGUI::MENU;
 //}
@@ -127,127 +119,130 @@ TypeGUI GUI::getTypeGUI() const {
 //SDL_Rect VolumeOptionGUI::getRectOfBtnBackToMenu() {
 //	return SDL_Rect();
 //}
-
-GamePlayGUI::GamePlayGUI() {
-
+GamePlayGUI::GamePlayGUI() { //LOAD ALL THE ESSENTIALS
+	SDL_RenderClear(Window::renderer);
 	this->active = true;
-	this->piece.clear();
-	this->possibleMove.clear();
-	//
-	this->board = Image({ 0,0,560,560 }, "..\\Assets\\Chess\\ChessBoard.png");
+	this->background = Image({ 0,0,1000,600 }, "..\\Assets\\chess\\back.jpg");
+	this->chosenPiece = nullptr;
+	//load board image
+	this->board = Image({ 0,0,560,560 }, "..\\Assets\\chess\\ChessBoard.png");
+	//load possible move image
+	Image possibleMove = Image({ 0,0,0,0 }, "..\\Assets\\PossibleMove.png");
 	//buttons
-	this->btnSetting = Image({ 800,20,100,100 }, "..\\Assets\\Chess\\settings.png");
-	this->btnUndo = Image({ 800,140,100,100 }, "..\\Assets\\Chess\\undo.png");
-	this->btnRedo = Image({ 800,260,100,100 }, "..\\Assets\\Chess\\redo.png");
-	this->btnQuit = Image({ 800,380,100,100 }, "..\\Assets\\Chess\\quit.png");
+	this->btnSetting = Image({ 800,20,100,100 }, "..\\Assets\\chess\\settings.png");
+	this->btnUndo = Image({ 800,140,100,100 }, "..\\Assets\\chess\\undo.png");
+	this->btnRedo = Image({ 800,260,100,100 }, "..\\Assets\\chess\\redo.png");
+	this->btnQuit = Image({ 800,380,100,100 }, "..\\Assets\\chess\\quit.png");
 	//promotion buttons
-	this->btnPromoteQueen = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProQueen.png");
-	this->btnPromoteBishop = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProBishop.png");
-	this->btnPromoteRook = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProRook.png");
-	this->btnPromoteKnight = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProKnight.png");
-}
-
-GamePlayGUI::~GamePlayGUI() {
-
-}
-
-void GamePlayGUI::init(Board* board) {
-	//get image of pieces
-	for (int i = 0; i < board->piecesOnBoard.size(); i++) {
-		for (int j = 0; j < board->piecesOnBoard[i].size(); j++) {
-			Piece* temppiece = board->piecesOnBoard[i][j];
-			if (temppiece) {
-				//get rect
-				int x = temppiece->getPosition().getX() * 70;
-				int y = temppiece->getPosition().getY() * 70;
-				//
-				std::cout << x << y << std::endl;
-				//
-				SDL_Rect temprect = { x,y,70,70 };
-				//get name
-				std::string tempstring = "..\\Assets\\Chess\\";
-				Color color = temppiece->getColor();
-				PieceType pt = temppiece->getType();
-				if (color == Color::Black) {
-					if (pt == PieceType::King) tempstring += "KingBlack.png";
-					if (pt == PieceType::Queen) tempstring += "QueenBlack.png";
-					if (pt == PieceType::Bishop) tempstring += "BishopBlack.png";
-					if (pt == PieceType::Rook) tempstring += "RookBlack.png";
-					if (pt == PieceType::Knight) tempstring += "KnightBlack.png";
-					if (pt == PieceType::Pawn) tempstring += "PawnBlack.png";
-					std::cout << tempstring << std::endl;
-				}
-				else {
-					if (pt == PieceType::King) tempstring += "KingWhite.png";
-					if (pt == PieceType::Queen) tempstring += "QueenWhite.png";
-					if (pt == PieceType::Bishop) tempstring += "BishopWhite.png";
-					if (pt == PieceType::Rook) tempstring += "RookWhite.png";
-					if (pt == PieceType::Knight) tempstring += "KnightWhite.png";
-					if (pt == PieceType::Pawn) tempstring += "PawnWhite.png";
-					std::cout << tempstring << std::endl;
-				}
-				Image temp = Image(temprect, tempstring);
-				this->piece.push_back(temp);
-			}
+	this->btnPromoteQueen = Image({ 0,0,0,0 }, "..\\Assets\\chess\\ProQueen.png");
+	this->btnPromoteBishop = Image({ 0,0,0,0 }, "..\\Assets\\chess\\ProBishop.png");
+	this->btnPromoteRook = Image({ 0,0,0,0 }, "..\\Assets\\chess\\ProRook.png");
+	this->btnPromoteKnight = Image({ 0,0,0,0 }, "..\\Assets\\chess\\ProKnight.png");
+	//LOAD PIECES IMAGEs
+	for (int i = 0; i < Board::piecesList.size(); i++) {
+		//get name
+		std::string tempstring = "..\\Assets\\chess\\";
+		Color color = Board::piecesList[i]->getColor();
+		PieceType pt = Board::piecesList[i]->getType();
+		if (color == Color::Black) {
+			if (pt == PieceType::King) tempstring += "KingBlack.png";
+			if (pt == PieceType::Queen) tempstring += "QueenBlack.png";
+			if (pt == PieceType::Bishop) tempstring += "BishopBlack.png";
+			if (pt == PieceType::Rook) tempstring += "RookBlack.png";
+			if (pt == PieceType::Knight) tempstring += "KnightBlack.png";
+			if (pt == PieceType::Pawn) tempstring += "PawnBlack.png";
+			std::cout << tempstring << std::endl;
 		}
-	}
-	//get image of possible moves
-	Piece* temppiece = nullptr;
-	for (int i = 0; i < board->piecesOnBoard.size(); i++) {
-		for (int j = 0; j < board->piecesOnBoard[i].size(); j++) {
-			temppiece = board->piecesOnBoard[i][j];
-			if (temppiece) if (temppiece->getChosen()) break;
+		else {
+			if (pt == PieceType::King) tempstring += "KingWhite.png";
+			if (pt == PieceType::Queen) tempstring += "QueenWhite.png";
+			if (pt == PieceType::Bishop) tempstring += "BishopWhite.png";
+			if (pt == PieceType::Rook) tempstring += "RookWhite.png";
+			if (pt == PieceType::Knight) tempstring += "KnightWhite.png";
+			if (pt == PieceType::Pawn) tempstring += "PawnWhite.png";
+			std::cout << tempstring << std::endl;
 		}
-		if (temppiece) if (temppiece->getChosen()) break;
-	}
-	if (!temppiece || !temppiece->getChosen()) return;
-	for (int i = 0; i < temppiece->getPossibleMoves().size(); i++) {
-		int x = temppiece->getPossibleMoves()[i].getX() * 70;
-		int y = temppiece->getPossibleMoves()[i].getY() * 70;
-		Image temp = Image({ x,y,70,70 }, "..\\Assets\\Chess\\PossibleMove.png");
+		int x = Board::piecesList[i]->getPosition().getX() * 70;
+		int y = Board::piecesList[i]->getPosition().getY() * 70;
+		Image temp = Image({ x,y,70,70 }, tempstring);
 		this->piece.push_back(temp);
 	}
 }
 
-void GamePlayGUI::render(SDL_Renderer* renderer) {
-	this->renderBackground(renderer);
-	//
-	this->board.renderImage(renderer);
-	//
-	for (int i = 0; i < this->possibleMove.size(); i++) {
-		this->possibleMove[i].renderImage(renderer);
+GamePlayGUI::~GamePlayGUI() {
+	this->destroy();
+}
+
+void GamePlayGUI::set() {
+	//set chosen one
+	for (int i = 0; i < Board::piecesList.size(); i++) {
+		if (!Board::piecesList[i]->getDead())
+		if (Board::piecesList[i]->getChosen()) {
+			this->chosenPiece = Board::piecesList[i];
+			break;
+		}
 	}
+	//set coor of pieces
+	for (int i = 0; i < Board::piecesList.size(); i++) {
+		if (Board::piecesList[i]->getDead()) {
+			this->piece[i].setRectangle({ 0,0,0,0 });
+		}
+		else {
+			int x = Board::piecesList[i]->getPosition().getX() * 70;
+			int y = Board::piecesList[i]->getPosition().getY() * 70;
+			this->piece[i].setRectangle({ x,y,70,70 });
+		}
+	}
+}
+
+void GamePlayGUI::render() {
+	//background
+	this->background.renderImage();
+	//board
+	this->board.renderImage();
+	//BUTTONS
+	this->btnUndo.renderImage();
+	this->btnRedo.renderImage();
+	this->btnSetting.renderImage();
+	this->btnQuit.renderImage();
+	//
+	this->btnPromoteQueen.renderImage();
+	this->btnPromoteBishop.renderImage();
+	this->btnPromoteRook.renderImage();
+	this->btnPromoteKnight.renderImage();
+	std::cout << "done\n";
+	//possible moves
+	if (this->chosenPiece != nullptr) 
+	for (int i = 0; i < this->chosenPiece->getPossibleMoves().size(); i++) {
+		int x = this->chosenPiece->getPossibleMoves()[i].getX() * 70;
+		int y = this->chosenPiece->getPossibleMoves()[i].getY() * 70;
+		this->possibleMove.setRectangle({ x,y,70,70 });
+		this->possibleMove.renderImage();
+	}
+	std::cout << "done\n";
+	//pieces
 	for (int i = 0; i < this->piece.size(); i++) {
-		this->piece[i].renderImage(renderer);
+		this->piece[i].renderImage();
 	}
-	//
-	this->btnUndo.renderImage(renderer);
-	this->btnRedo.renderImage(renderer);
-	this->btnSetting.renderImage(renderer);
-	this->btnQuit.renderImage(renderer);
-	//
-	this->btnPromoteQueen.renderImage(renderer);
-	this->btnPromoteBishop.renderImage(renderer);
-	this->btnPromoteRook.renderImage(renderer);
-	this->btnPromoteKnight.renderImage(renderer);
+	std::cout << "done\n";
+	SDL_RenderPresent(Window::renderer);
 }
 void GamePlayGUI::initPromotionButtons() {
-	this->btnPromoteQueen = Image({ 570,300,70,70 }, "..\\Assets\\Chess\\ProQueen.png");
-	this->btnPromoteBishop = Image({ 570,380,70,70 }, "..\\Assets\\Chess\\ProBishop.png");
-	this->btnPromoteRook = Image({ 650,300,70,70 }, "..\\Assets\\Chess\\ProRook.png");
-	this->btnPromoteKnight = Image({ 650,380,70,70 }, "..\\Assets\\Chess\\ProKnight.png");
+	this->btnPromoteQueen.setRectangle({ 570,300,70,70 });
+	this->btnPromoteBishop.setRectangle({ 570,380,70,70 });
+	this->btnPromoteRook.setRectangle({ 650,300,70,70 });
+	this->btnPromoteKnight.setRectangle({ 650,380,70,70 });
 }	 //call when needed
 void GamePlayGUI::clearPromotionButtons() {
-	this->btnPromoteQueen = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProQueen.png");
-	this->btnPromoteBishop = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProBishop.png");
-	this->btnPromoteRook = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProRook.png");
-	this->btnPromoteKnight = Image({ 0,0,0,0 }, "..\\Assets\\Chess\\ProKnight.png");
+	this->btnPromoteQueen.setRectangle({ 0,0,0,0 });
+	this->btnPromoteBishop.setRectangle({ 0,0,0,0 });
+	this->btnPromoteRook.setRectangle({ 0,0,0,0 });
+	this->btnPromoteKnight.setRectangle({ 0,0,0,0 });
 }   //must call after use
 //get rect
 TypeGUI GamePlayGUI::getTypeGUI() const {
 	return TypeGUI::GAME_PLAY;
 }
-
 SDL_Rect GamePlayGUI::getRectOfBtnUndo() {
 	return this->btnUndo.getRectangle();
 }
@@ -255,14 +250,11 @@ SDL_Rect GamePlayGUI::getRectOfBtnUndo() {
 SDL_Rect GamePlayGUI::getRectOfBtnSetting() {
 	return this->btnSetting.getRectangle();
 }
-
 SDL_Rect GamePlayGUI::getRectOfBtnRedo() {
 	return this->btnRedo.getRectangle();
 }
-void GamePlayGUI::clear(SDL_Renderer* renderer) {
-	GUI::clear(renderer);
-	this->piece.clear();
-	this->possibleMove.clear();
+SDL_Rect GamePlayGUI::getRectOfBtnQuit() {
+	return this->btnQuit.getRectangle();
 }
 SDL_Rect GamePlayGUI::getRectOfBtnPromoteQueen() {
 	return this->btnPromoteQueen.getRectangle();
@@ -276,7 +268,24 @@ SDL_Rect GamePlayGUI::getRectOfBtnPromoteBishop() {
 SDL_Rect GamePlayGUI::getRectOfBtnPromoteKnight() {
 	return this->btnPromoteKnight.getRectangle();
 }
-
+void GamePlayGUI::destroy() {
+	for (int i = 0; i < this->piece.size(); i++) {
+		this->piece[i].destroy();
+	}
+	this->piece.clear();
+	//
+	this->btnPromoteQueen.destroy();
+	this->btnPromoteRook.destroy();
+	this->btnPromoteBishop.destroy();
+	this->btnPromoteKnight.destroy();
+	this->btnQuit.destroy();
+	this->btnUndo.destroy();
+	this->btnRedo.destroy();
+	this->btnSetting.destroy();
+	this->possibleMove.destroy();
+	//
+	this->board.destroy();
+}
 //MatchResultGUI::MatchResultGUI() {
 //}
 //
